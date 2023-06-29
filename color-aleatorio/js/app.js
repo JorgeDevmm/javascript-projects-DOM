5fa103e3a4928be369ccb5e5bdd5b99d4b65f1a6
JorgeDevmm <jorge29luis10@gmail.com>
2023-06-28 23:31:19 -0500

    agregando detalles

--- color-aleatorio/js/app.js
+++ color-aleatorio/js/app.js
@@ -7,19 +7,22 @@ const ColorAleatorioHex = () => {
   let colorHex = '#';
 
   for (let i = 0; i < 6; i++) {
+    // genera un numero pseudoaleatorio
     let indiceAleatorio = Math.floor(Math.random() * 16);
 
+    // guardamos lov valore de los 6 indices indice en la variable
     colorHex += digitos[indiceAleatorio];
   }
   return colorHex;
 };
 
 boton.addEventListener('click', () => {
+  // obtenemos el color hexa de la función y lo almacenamos en la variable
   let colorAleatorio = ColorAleatorioHex();
 
-  console.log(colorAleatorio);
-
-  texto.innerText = colorAleatorio;
-
+  // modificamos el background
   body.style.backgroundColor = colorAleatorio;
+
+  // modificamos el texto
+  texto.textContent = colorAleatorio;
 });
